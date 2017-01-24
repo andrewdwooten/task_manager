876efcfea12a618d776051bc895808b9c33ca4fb
Andy Wooten <andrewdwooten@icloud.com>
2017-01-23 21:37:36 -0700

    Homework Day 1: Built-in routes, view, and methods to delete as well as edit tasks

--- app/controllers/task_manager_app.rb
+++ app/controllers/task_manager_app.rb
@@ -1,9 +1,10 @@
 require_relative '../models/task.rb'
 
-class TaskManagerApp < Sinatra::Base
-  set :root, File.expand_path("..", __dir__)
+class TaskManagerApp < Sinatra::Base #without sinatra base; this does not work
+  set :root, File.expand_path("..", __dir__) #sets app folder as root and where sinatra starts looking fo things.
+  set :method_override, true
 
-  get '/' do
+  get '/' do    #rendering language; when '/' do this
     erb :dashboard
   end
 
@@ -25,5 +26,20 @@ class TaskManagerApp < Sinatra::Base
   get '/tasks/:id' do
     @task = Task.find(params[:id])
     erb :show
+  end
+
+  get '/tasks/:id/edit' do
+    @task = Task.find(params[:id])
+    erb :edit
+  end
+
+  put '/tasks/:id' do |id|
+    Task.update(id.to_i, params[:task])
+    redirect "/tasks/#{id}"
+  end
+
+  delete '/tasks/:id' do |id|
+    Task.destroy(id.to_i)
+    redirect '/tasks'
   end
 end
